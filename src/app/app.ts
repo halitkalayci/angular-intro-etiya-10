1bd84029b9ba47cc38bd7612df027af8bb69d0c6
halitklyc <halitenes.kalayci@gmail.com>
2025-10-20 11:45:11 +0300

    Add dynamic class handling and example component
    - Implement dynamic class binding in the main app component.
    - Update button click behavior to change the name and class.
    - Create example component with basic template and styles.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,25 @@
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { Example } from './example/example';
 
 @Component({
   selector: 'app-root', // bu component başka componentlarda <app-root></app-root> şeklinde kullanılır
   templateUrl: './app.html', // HTML şablon dosyasının yolu
-  styleUrl: './app.scss' // Stil dosyasının yolu
+  styleUrl: './app.scss', // Stil dosyasının yolu
+  imports: [FormsModule, Example], // bu componentin kullandığı diğer componentler, direktifler veya pipe'lar
 })
 export class App {
   public name: string = "Halit";
+  public dynamicClass :string = "my-btn";
 
   onClick() {
     alert("Butona tıklandı!");
+    this.name = "Ahmet";
+  }
+
+  submit() {
+    alert(this.name);
+    this.dynamicClass = "my-btn-clicked";
   }
 }
