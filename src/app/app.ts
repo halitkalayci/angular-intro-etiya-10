8e60a310e30303ecd67b14e668581596d899270b
halitklyc <halitenes.kalayci@gmail.com>
2025-10-20 11:24:04 +0300

    değişken kullanma ve aksiyon atama.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  selector: 'app-root', // bu component başka componentlarda <app-root></app-root> şeklinde kullanılır
+  templateUrl: './app.html', // HTML şablon dosyasının yolu
+  styleUrl: './app.scss' // Stil dosyasının yolu
 })
 export class App {
-  protected readonly title = signal('angular-intro');
+  public name: string = "Halit";
+
+  onClick() {
+    alert("Butona tıklandı!");
+  }
 }
