30f25eabb3ab39789d949952fdf6620ab1e47ec2
halitklyc <halitenes.kalayci@gmail.com>
2025-10-20 11:48:45 +0300

    Add comments to the Example component for project structure guidance

--- src/app/example/example.ts
+++ src/app/example/example.ts
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './example.scss'
 })
 export class Example {
-
+  // Kendinize bir tanıtım web sitesi oluşturduğunuzu hayal edin.
+  // İçerisinde hakkımda, projelerim ve iletişim bilgilerim gibi bölümler bulunuyor.
+  // Componentlara bölerek bu projeyi nasıl yapılandırırdınız?
+  // İletişim formu zorunlu. (Girilen tüm bilgileri konsola yazsın.)
 }
