826e2fab60ebaeb9176226c331280ffa551d7266
halitklyc <halitenes.kalayci@gmail.com>
2025-10-21 11:12:41 +0300

    ngOnInit();

--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductCard } from '../product-card/product-card';
 import { Product } from '../../models/product';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-product-list',
@@ -9,11 +10,21 @@ import { CommonModule } from '@angular/common';
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss'
 })
-export class ProductList {
+//  implements OnInit => opsiyonel ama faydalı (yazım yanlışlarına karşı.)
+export class ProductList implements OnInit {
   products:Product[]=[
     {id:1,name:"Ürün 1", price:500, stock:100, imgUrl: "https://placehold.co/600x400?text=%C3%9Cr%C3%BCn+1"},
     {id:2,name:"Ürün 2", price:1500, stock:250},
     {id:3,name:"Ürün 3", price:500, stock:100},
     {id:4,name:"Ürün 4", price:600, stock:250}
   ]
+
+  // Ctor parametreleri ekstra parametrelerle açılmak zorunda değil otomatik this. altına eklenir.
+  constructor(private httpClient:HttpClient) {}
+
+  ngOnInit() {
+    console.log("ProductList.ngOnInit çalıştı")
+  }
+
+  fetchProducts() {}
 }
