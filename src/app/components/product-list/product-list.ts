a4d313768722c47dd328b639d7a6945dbf1a6422
halitklyc <halitenes.kalayci@gmail.com>
2025-10-21 10:55:58 +0300

    @if

--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -11,7 +11,7 @@ import { CommonModule } from '@angular/common';
 })
 export class ProductList {
   products:Product[]=[
-    {id:1,name:"Ürün 1", price:500, stock:100},
+    {id:1,name:"Ürün 1", price:500, stock:100, imgUrl: "https://placehold.co/600x400?text=%C3%9Cr%C3%BCn+1"},
     {id:2,name:"Ürün 2", price:1500, stock:250},
     {id:3,name:"Ürün 3", price:500, stock:100},
     {id:4,name:"Ürün 4", price:600, stock:250}
