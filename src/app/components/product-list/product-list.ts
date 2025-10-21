9c7cffdbf389e357dd95155b2d039e8a245d1733
halitklyc <halitenes.kalayci@gmail.com>
2025-10-21 10:39:52 +0300

    *ngFor

--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ProductCard } from '../product-card/product-card';
 import { Product } from '../../models/product';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-list',
-  imports: [ProductCard],
+  imports: [CommonModule,ProductCard],
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss'
 })
@@ -12,5 +13,7 @@ export class ProductList {
   products:Product[]=[
     {name:"Ürün 1", price:500, stock:100},
     {name:"Ürün 2", price:1500, stock:250},
+    {name:"Ürün 3", price:500, stock:100},
+    {name:"Ürün 4", price:600, stock:250}
   ]
 }
