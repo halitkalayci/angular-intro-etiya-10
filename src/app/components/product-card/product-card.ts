ed69a205ae5d560f8c48d8d04e1e8ebe7162c39f
halitklyc <halitenes.kalayci@gmail.com>
2025-10-22 11:07:07 +0300

    pipes

--- src/app/components/product-card/product-card.ts
+++ src/app/components/product-card/product-card.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { Product } from '../../models/productListResponse';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-card.html',
   styleUrl: './product-card.scss'
 })
 export class ProductCard {
   @Input() product!:Product;
 }
+// ALT GR + T => ₺
--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ProductList } from "../../components/product-list/product-list";
 
 @Component({
   selector: 'app-products',
-  imports: [RouterLink],
+  imports: [RouterLink, ProductList],
   templateUrl: './products.html',
   styleUrl: './products.scss'
 })
