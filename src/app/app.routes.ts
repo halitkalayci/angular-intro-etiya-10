fabc07bdad4baba5f56ea42affd7cfde3fab79ee
halitklyc <halitenes.kalayci@gmail.com>
2025-10-27 09:42:04 +0300

    layout eklendi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,22 @@ import { Routes } from '@angular/router';
 import { Homepage } from './pages/homepage/homepage';
 import { Products } from './pages/products/products';
 import { Login } from './pages/login/login';
+import { MainLayout } from './layouts/main-layout/main-layout';
 
 export const routes: Routes = [
-    {path:'', redirectTo:'homepage', pathMatch:'full'},
-    {path:'homepage', component:Homepage},
-    {path:'products', component:Products},
-    {path:'login', component:Login}
+    // Parent-Child
+    {path:'login', component:Login},
+    {
+        path: '',
+        component: MainLayout,
+        children: [
+                {path:'', redirectTo:'homepage', pathMatch:'full'},
+                {path:'homepage', component:Homepage},
+                {path:'products', component:Products}
+        ]
+    },
+    {
+        path:'**',
+        redirectTo:'homepage'
+    }
 ];
